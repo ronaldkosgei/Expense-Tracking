c2b8c00075b14e1ef169a6ccb7ef7432cfbdd762
Ronald Kosgei <kosgeironald4@gmail.com>
2023-07-13 13:08:57 +0300

    Budget Manager

---
+++ src/main/java/com/ExpenseTracking/ExpenseTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.ExpenseTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ExpenseTracking/ExpenseTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ExpenseTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
